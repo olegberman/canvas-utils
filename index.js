4b48aba34cf85e590fed3804ca5d99d801e7ae9d
Oleg Berman <bermanoleg@gmail.com>
2016-05-13 16:34:01 -0400

    ES5 version + fixes

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 /**
-  There's no native support of mulitline text in Canvas,
-  this function will help you to draw multiline text
-  by providing max width, line breaks will happen there
+-  There's no native support of mulitline text in Canvas,
+-  this function will help you to draw multiline text
+-  by providing max width, line breaks will happen there
 **/
 
-const wrap_text = (ctx, text, x, y, lineHeight, maxWidth) => {
-  const words = text.split(' ')
-  let lines = []
-  let sliceFrom = 0
-  for(let i = 0; i < words.length; i++) {
-    let chunk = words.slice(sliceFrom, i).join(' ')
-    const last = i === words.length - 1
-    const bigger = ctx.measureText(chunk).width > maxWidth
-    if(bigger || last) {
-      lines.push(words.slice(sliceFrom, i - 1).join(' '))
+var wrap_text = (ctx, text, x, y, lineHeight, maxWidth, textAlign) => {
+  if(!textAlign) textAlign = 'center'
+  ctx.textAlign = textAlign
+  var words = text.split(' ')
+  var lines = []
+  var sliceFrom = 0
+  for(var i = 0; i < words.length; i++) {
+    var chunk = words.slice(sliceFrom, i).join(' ')
+    var last = i === words.length - 1
+    var bigger = ctx.measureText(chunk).width > maxWidth
+    if(bigger) {
+      lines.push(words.slice(sliceFrom, i).join(' '))
+      sliceFrom = i
+    }
+    if(last) {
+      lines.push(words.slice(sliceFrom, words.length).join(' '))
       sliceFrom = i
     }
   }
-  let offsetY = 0
-  for(let i = 0; i < lines.length; i++) {
-    ctx.fillText(lines[i], x, y + offsetY)
+  var offsetY = 0
+  var offsetX = 0
+  if(textAlign === 'center') offsetX = maxWidth / 2
+  for(var i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], x + offsetX, y + offsetY)
     offsetY = offsetY + lineHeight
   }
 }
